733eb504fde0d75538322342525f843337d12889
Hildebrando Viana Matos <hildocontato@gmail.com>
2022-03-25 18:59:11 -0300

    Changing the name files to HTML

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 	if(isset($_GET['url'])){
-		if(file_exists($_GET['url'].'.php')){
-			include($_GET['url'].'.php');
+		if(file_exists($_GET['url'].'.html')){
+			include($_GET['url'].'.html');
 		}else{
-			include('404.php');
+			include('404.html');
 		}
 	}else{
-		include('home.php');
+		include('home.html');
 	}
 ?>
